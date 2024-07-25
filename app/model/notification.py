3d3b2ce94e15718dd9eefae66af359171bf91122
PinYi Lee(李品毅) <PinYi_Lee@T-01010549ML1.local>
2024-07-25 23:27:26 +0800

    add req field_validator

--- app/model/notification.py
+++ app/model/notification.py
@@ -1,4 +1,11 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field , field_validator
+from uuid import UUID
 
 class PublishRequest(BaseModel):
     friend_id: str = Field(..., description = "friend id")
+    
+    @field_validator('friend_id')
+    def validate_user_id(cls, value):
+        if not isinstance(value, UUID):
+            raise ValueError(f"{value} is not a valid UUID")
+        return value
